bb112a2935171d3eb1eb383a25fd1e39a7a03126
hasegawa <hasegawa@localhost>
2025-04-01 12:15:22 +0900

    Minor modification.

--- src/main/java/yokwe/family/register/type/FamilyRegister.java
+++ src/main/java/yokwe/family/register/type/FamilyRegister.java
@@ -1,9 +1,28 @@
 package yokwe.family.register.type;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class FamilyRegister {
 	private static final String UNKNOWN = "不明";
 	
 	public static boolean isUnknown(String string) {
 		return string.equals(UNKNOWN);
+	}
+	
+	
+	private List<Event> eventList;
+
+	public List<Event> getEvent(Event.Type type) {
+		return null;
+	}
+	public List<Event> getEvent(String name) {
+		return eventList.stream().filter(o -> o.name.equals(name)).collect(Collectors.toList());
+	}
+	
+	private List<Person> personList;
+
+	public Person getPerson(String name) {
+		return personList.stream().filter(o -> name.equals(o.lastName + o.firstName)).findFirst().orElse(null);
 	}
 }
